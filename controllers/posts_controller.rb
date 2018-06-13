ec42b9887181122853261ff238553b8e43868fa3
Tech-A43 <tech-a43@Tech-A43s-MacBook-Air.local>
2018-06-13 15:50:05 +0100

    Lesson finished

--- controllers/posts_controller.rb
+++ controllers/posts_controller.rb
@@ -28,22 +28,88 @@ class PostsController < Sinatra::Base # base module allows us to use HTTP verbs
     }
   ]
 
-  # root
-  get '/posts' do
-
+  # root - index
+  get '/' do
     @title = "Posts Index" # global variable, use them sparingly
+    @posts = $posts
 
     erb :'posts/index'
   end
 
-  get '/posts/:id' do
+  # new
+  get '/new' do
+    @title = "New Post" # global variable, use them sparingly
 
+    @post = {
+      :id => "",
+      :title => "",
+      :body => ""
+    }
+
+    erb :'posts/new'
+  end
+
+  # show
+  get '/:id' do
     id = params[:id].to_i
 
     @title = "Show Post"
     @post = $posts[id]
 
     erb :'posts/show'
+  end
+
+  # create
+  post '/' do
+    # puts params # on submit, form data submits to URL
+
+    id = $posts.last[:id] + 1
+
+    new_post = {
+      :id => id,
+      :title => params[:title],
+      :body => params[:body]
+    }
+
+    puts new_post
+    $posts.push new_post
+
+    redirect '/' # redirect is a get request
+  end
+
+  # edit
+  get '/:id/edit' do
+    id = params[:id].to_i
+
+    @post = $posts[id]
+    @title = @post[:title]
+    # @title = "Edit Page #{id+1}"
+
+    erb :'posts/edit'
+  end
+
+  # update
+  put '/:id' do
+
+    id = params[:id].to_i
+
+    post = $posts[id] # get hash from array
+
+    post[:title] = params[:title] # update hash with values from URL/params
+    post[:body] = params[:body]
+
+    $posts[id] = post # save new data back into array
+
+    redirect '/'
+  end
+
+  # delete
+  delete '/:id' do
+    id = params[:id].to_i
+
+    $posts.delete_at id
+
+    redirect '/'
   end
 
 end
