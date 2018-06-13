d112742736bcca8dd4bb97604a50ea8a033eae66
Tech-A43 <tech-a43@Tech-A43s-MacBook-Air.local>
2018-06-13 10:33:23 +0100

    Used ERB to bring ruby syntax into html

--- controllers/posts_controller.rb
+++ controllers/posts_controller.rb
@@ -4,8 +4,36 @@ class PostsController < Sinatra::Base # base module allows us to use HTTP verbs
     register Sinatra::Reloader
   end
 
+  # set root to parent directory of current file
+  set :root, File.join(File.dirname(__FILE__), '..')
+
+  # sets the view directory corretly
+  set :views, Proc.new { File.join(root, "views") }
+
+  $posts = [
+    {
+      :id => 0,
+      :title => "Post 1",
+      :body => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+      :id => 1,
+      :title => "Post 2",
+      :body => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+      :id => 2,
+      :title => "Post 3",
+      :body => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    }
+  ] # global variable, use them sparingly
+
+  # root
   get '/' do
-    "Hello World!"
+
+    @title = "Posts Index"
+
+    erb :'posts/index'
   end
 
 end
